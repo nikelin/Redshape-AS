8d261021e2e6603097a850b256e5e4634be2e38c
Cyril A. Karpenko <self@nikelin.ru>
2012-02-20 09:21:55 +0400

    * FormPanel used to render IForm

--- renderers/renderers-forms-gwt/src/main/java/com/redshape/renderer/forms/renderers/StandardGWTFormRenderer.java
+++ renderers/renderers-forms-gwt/src/main/java/com/redshape/renderer/forms/renderers/StandardGWTFormRenderer.java
@@ -1,14 +1,16 @@
 package com.redshape.renderer.forms.renderers;
 
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.redshape.form.IForm;
 import com.redshape.form.IFormField;
+import com.redshape.form.IFormItem;
 import com.redshape.renderer.IRenderersFactory;
 import com.redshape.renderer.forms.renderers.decorators.ErrorsDecorator;
 import com.redshape.renderer.forms.renderers.decorators.FormFieldDecorator;
 import com.redshape.renderer.forms.renderers.decorators.LabelDecorator;
+import com.redshape.renderer.forms.renderers.decorators.LegendDecorator;
 import com.redshape.utils.Commons;
 
 /**
@@ -41,32 +43,42 @@ public class StandardGWTFormRenderer extends AbstractGWTRenderer<IForm> {
     @Override
     public Widget render(IForm renderable) {
         Commons.checkNotNull(renderable);
-        
+
+        FormPanel form = new FormPanel();
+        this.buildAttributes(form, renderable);
+        this.checkDecorators(renderable);
+        form.setAction(Commons.select(renderable.getAction(), "#" ));
+        form.setMethod(Commons.select(renderable.getMethod(), "POST" ));
+
         VerticalPanel panel = new VerticalPanel();
-        this.buildAttributes(panel, renderable);
-        panel.setTitle( renderable.getLegend() );
-        panel.add( new Label(renderable.getLegend()) );
+        form.add(panel);
+        panel.setTitle(renderable.getLegend());
         
         for (IFormField<?> field : renderable.getFields() ) {
             this.checkDecorators(field);
             panel.add( this.getRenderersFactory().<IFormField, Widget>forEntity(field).render(field) );
         }
 
-        this.applyDecorators(renderable, panel);
+    
+        this.applyDecorators(renderable, form );
 
-        return panel;
+        return form;
     }
 
-    protected void checkDecorators( IFormField<?> field ) {
-        if ( !field.hasDecorator(LabelDecorator.class) ) {
+    protected void checkDecorators( IFormItem field ) {
+        if ( field instanceof IFormField && !field.hasDecorator(LabelDecorator.class) ) {
             field.setDecorator( new LabelDecorator() );
+        }
+
+        if ( field instanceof IForm && !field.hasDecorator(LegendDecorator.class) ) {
+            field.setDecorator( new LegendDecorator() );
         }
 
         if ( !field.hasDecorator(ErrorsDecorator.class) ) {
             field.setDecorator( new ErrorsDecorator() );
         }
 
-        if ( !field.hasDecorator( FormFieldDecorator.class ) ) {
+        if ( field instanceof IFormField && !field.hasDecorator( FormFieldDecorator.class ) ) {
             field.setDecorator( new FormFieldDecorator() );
         }
     }
