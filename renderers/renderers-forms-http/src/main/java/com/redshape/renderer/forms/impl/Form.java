ab0adc48a378c266aaaaadd7a79e9640b9784e3c
Cyril A. Karpenko <self@nikelin.ru>
2012-04-04 21:46:29 +0400

    * POST and GET HTTP methods constants in the Form class

--- renderers/renderers-forms-http/src/main/java/com/redshape/renderer/forms/impl/Form.java
+++ renderers/renderers-forms-http/src/main/java/com/redshape/renderer/forms/impl/Form.java
@@ -8,7 +8,9 @@ import com.redshape.renderer.forms.decorators.ErrorsDecorator;
  * @date 2/15/12 {6:55 PM}
  */
 public class Form extends com.redshape.form.impl.Form {
-
+    public static final String POST = "POST";
+    public static final String GET = "GET";
+    
     public Form() {
         this(null);
     }
