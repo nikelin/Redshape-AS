c48925bbd9adaa3ab2b6240b6985a8ea6281946b
Cyril A. Karpenko <self@nikelin.ru>
2012-05-17 15:03:37 +0400

    * Hashers moved to API level of utils ( because GWT really provides java.security.* package )

--- utils/utils-api/src/main/java/com/redshape/utils/hashers/SHA1.java
+++ utils/utils-api/src/main/java/com/redshape/utils/hashers/SHA1.java
@@ -1,21 +1,15 @@
 package com.redshape.utils.hashers;
 
+import com.redshape.utils.Base64;
 import com.redshape.utils.IHasher;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- * Created by IntelliJ IDEA.
- * User: flare
- * Date: 18.06.2010
- * Time: 17:41:11
- * To change this template use File | Settings | File Templates.
- */
-public class MD5 implements IHasher {
+public class SHA1 implements IHasher {
 
     public String getName() {
-        return "MD5";
+        return "SHA1";
     }
 
     private static String convertToHex(byte[] data) {
@@ -24,12 +18,13 @@ public class MD5 implements IHasher {
             int halfbyte = (data[i] >>> 4) & 0x0F;
             int two_halfs = 0;
             do {
-                if ( (0 <= halfbyte) && (halfbyte <= 9) ) {
+                if ((0 <= halfbyte) && (halfbyte <= 9)) {
                     buf.append((char) ('0' + halfbyte));
                 }
                 else {
                     buf.append((char) ('a' + (halfbyte - 10)));
                 }
+
                 halfbyte = data[i] & 0x0F;
             } while(two_halfs++ < 1);
         }
@@ -47,24 +42,23 @@ public class MD5 implements IHasher {
     @Override
     public String hashBase64( Object text ) {
         try {
-            return new sun.misc.BASE64Encoder().encode( this.digitize(text) );
+            return new String( Base64.encode( this.digitize(text) ) );
         } catch ( Throwable e ) {
             throw new IllegalStateException( e.getMessage(), e );
         }
     }
 
     protected byte[] digitize( Object text ) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
         md.update( String.valueOf( text ).getBytes(), 0, String.valueOf( text ).length());
         return md.digest();
     }
 
-    @Override
     public String hash( Object text ) {
         try {
             return convertToHex( this.digitize(text) );
         } catch ( Throwable e ) {
-            return null;
+            throw new IllegalStateException( e.getMessage(), e );
         }
     }
 }
