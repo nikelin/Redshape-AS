edec663a104dbf6fcc7a166ceda996f5ededea5f
Cyril A. Karpenko <self@nikelin.ru>
2012-07-23 18:41:53 +0400

    * Make file fixed

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
 export M2_HOME=/usr/share/maven &&
-/usr/share/maven/bin/mvn clean install -DpersistenceAll=enabled -DuiAll=enabled -DrenderersAll=enabled
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m" &&
+/usr/share/maven/bin/mvn  clean install -PnetAll,renderers-all,persistence-all,uiAll,jobsAll
