3b59e76033245c8afd5a19082d448667fba781f7
sergey.kidyaev <sergey.kidyaev@innovationlab.ru>
2012-07-27 22:24:42 +0400

    Class com.redshape.utils.system.console.Console refactored.  - mkdir(String path) realized  - deleteFile(String path) refactored

--- utils/utils-core/src/main/java/com/redshape/utils/system/console/Console.java
+++ utils/utils-core/src/main/java/com/redshape/utils/system/console/Console.java
@@ -33,18 +33,31 @@ public class Console implements IConsole {
 
     @Override
     public void mkdir(String path) throws IOException {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if ( checkExists(path) )
+            return;
+
+        String command = "mkdir -p " + path;
+
+        // todo: synchronize on path?
+        executeCommand(path, command);
+
+
     }
 
     @Override
 	public void deleteFile(String path) throws IOException {
-		File file = this.<File>provideFile(path, false);
-		if ( !file.exists() ) {
-			return;
-		}
+        if ( !checkExists(path) )
+            return;
 
-		file.delete();
-	}
+        String command = "rm -r " + path;
+
+        // todo: synchronize on path?
+        executeCommand(path, command);
+    }
+
+    private String executeCommand(Object context, String command) throws IOException {
+        return createExecutor(context, command).execute();
+    }
 
 	@Override
 	public boolean checkExists(String path) throws IOException {
---
+++ utils/utils-core/src/test/java/com/redshape/utils/system/console/ConsoleTest.java
@@ -0,0 +1,56 @@
+package com.redshape.utils.system.console;
+
+import static junit.framework.Assert.*;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * User: serge
+ * Date: 7/27/12
+ * Time: 6:00 PM
+ */
+public class ConsoleTest {
+
+    public static final String USER_HOME_DIR = System.getProperty("user.home");
+    public static final String SUBDIR_1_PATH = USER_HOME_DIR + File.separator + "subdir.1";
+    public static final String SUBDIR_2_PATH = SUBDIR_1_PATH + File.separator + "subdir.2";
+
+    @Test
+    public void testCheckExists() throws IOException {
+        IConsole console = new Console();
+        assertTrue(console.checkExists(USER_HOME_DIR));
+        assertFalse(console.checkExists(SUBDIR_1_PATH));
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void testMkdir() throws IOException {
+        IConsole console = new Console();
+
+        assertFalse( new File(SUBDIR_2_PATH).exists() );
+
+        console.mkdir(SUBDIR_2_PATH);
+
+        assertTrue(new File(SUBDIR_2_PATH).exists());
+
+        new File(SUBDIR_2_PATH).delete();
+        new File(SUBDIR_1_PATH).delete();
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void testDeleteFile() throws IOException {
+        new File(SUBDIR_1_PATH).mkdir();
+        new File(SUBDIR_2_PATH).mkdir();
+
+        IConsole console = new Console();
+
+        assertTrue( new File(SUBDIR_2_PATH).exists() );
+
+        console.deleteFile(SUBDIR_2_PATH);
+
+        assertFalse( new File(SUBDIR_2_PATH).exists() );
+    }
+}
