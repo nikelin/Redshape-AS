78a7d270147f6bab1583b46e41c5a0fdffe65d04
Cyril A. Karpenko <self@nikelin.ru>
2012-08-01 19:28:14 +0400

    * Little fix

--- utils/utils-api/src/main/java/com/redshape/utils/config/PropertiesConfig.java
+++ utils/utils-api/src/main/java/com/redshape/utils/config/PropertiesConfig.java
@@ -3,6 +3,11 @@ package com.redshape.utils.config;
 import com.redshape.utils.SimpleStringUtils;
 import com.redshape.utils.config.sources.IConfigSource;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Cyril A. Karpenko <self@nikelin.ru>
  * @package com.redshape.utils.config
@@ -10,6 +15,14 @@ import com.redshape.utils.config.sources.IConfigSource;
  */
 public class PropertiesConfig extends AbstractConfig {
 	public static final String[] STANDARD_LINE_DELIMITERS = new String[] { "\n", "\r", ";" };
+
+    protected boolean nulled;
+    protected String value;
+    protected IConfig parent;
+    protected String name;
+    protected Map<String, String> attributes = new LinkedHashMap<String, String>();
+    protected List<IConfig> childs = new ArrayList<IConfig>();
+    protected IConfigSource source;
 
     public PropertiesConfig() {
         this(null, null, null);
@@ -70,8 +83,8 @@ public class PropertiesConfig extends AbstractConfig {
 		return new PropertiesConfig(this, name, null);
 	}
 
-    public static IConfig fromSource( IConfig config ) throws ConfigException {
-        IConfig result = new PropertiesConfig( config.name(), config.value() );
+    public static PropertiesConfig fromSource( IConfig config ) throws ConfigException {
+        PropertiesConfig result = new PropertiesConfig( config.name(), config.value() );
         for ( IConfig child : config.childs() ) {
             result.append( fromSource(child) );
         }
