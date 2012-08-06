91fbd4085755d41c0ce3d5b1121753d5db6b246f
nikelin <nikelin@VT1.(none)>
2012-08-06 13:57:47 +0400

    Make fixed

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
-export M2_HOME=/usr/share/maven &&
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m" &&
-/usr/share/maven/bin/mvn  clean install -PnetAll,renderers-all,persistence-all,uiAll,jobsAll
+mvn  clean install -PnetAll,renderers-all,persistence-all,uiAll,jobsAll
