cb974442ca9a36e6d6d322e0df667eee2b3aa3f6
Cyril A. Karpenko <self@nikelin.ru>
2012-09-28 15:53:27 +0400

    Fixes

--- perform-release.sh
+++ perform-release.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-MAVEN_OPTS="-XX:MaxPermSize=512m -Xmx512m" mvn release:perform -PuiAll,netAll,renderers-all,persistence-all,jobsAll
+MAVEN_OPTS="-XX:MaxPermSize=512m -Xmx512m" mvn -X release:perform -PuiAll,netAll,renderers-all,persistence-all,jobsAll
