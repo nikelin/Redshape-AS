fe3b1faf05f9a875bc5505927f42df38418b2965
Cyril A. Karpenko <self@nikelin.ru>
2012-11-13 12:04:29 +0400

    * Copyrights fixed * LICENSE file provided * Project description fixed a few

--- ascript/src/main/java/com/redshape/ascript/context/items/ValueItem.java
+++ ascript/src/main/java/com/redshape/ascript/context/items/ValueItem.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Cyril A. Karpenko
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.redshape.ascript.context.items;
 
 import com.redshape.ascript.EvaluationException;
@@ -5,7 +21,6 @@ import com.redshape.ascript.context.IEvaluationContextItem;
 import com.redshape.utils.ILambda;
 
 import java.lang.reflect.Method;
-
 
 public class ValueItem implements IEvaluationContextItem {
 	private Object value;
